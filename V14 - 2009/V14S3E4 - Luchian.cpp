9fbef34c3e390edd830af3f3a5a26c053c684a51
Luchian67 <46937062+Luchian67@users.noreply.github.com>
2019-06-01 11:39:58 +0300

    Edit V14 S3 E4

--- V14 - 2009/V14S3E4 - Luchian.cpp
+++ V14 - 2009/V14S3E4 - Luchian.cpp
@@ -3,8 +3,17 @@
 
 using namespace std;
 
-void sub(ifstream &f, unsigned int &contor_nr_pare)
-{
+/*
+Fişierul  text  BAC.TXT  conţine  mai  multenumere  naturale,  cu  cel  mult  6 cifre fiecare, 
+câte un număr pe fiecare linie a fişierului.
+Scrieţi un program C/C++ care citeşte toate numerele din fişierul BAC.TXT şi le afişează pe ecran, 
+în aceeaşi ordine, câte cinci pe fiecare linie, separate prin câte un  spaţiu,  
+cu  excepţia  ultimei  linii  care  poate  conţine  mai  puţin  de  cinci  numere.    
+Programul va afişa apoi pe ecran, pe o linie separată,  câte numere din fişier au suma cifrelor pară.
+*/
+
+void sub(ifstream &f, unsigned int &contor_nr_pare) // acest subprogram citeste si afiseaza 5 numere din fisier
+{                                                   // si contorizeaza cate numere pare citeste
         unsigned int v[5];
 
         for (int i=0; i<5; i++)
@@ -22,8 +31,8 @@ int main()
         ifstream f("BAC.TXT");
         unsigned int contor_nr_pare=0;
 
-        while (f) sub(f, contor_nr_pare);
-        cout << contor_nr_pare;
+        while (f) sub(f, contor_nr_pare); // subprogramul este rulat pana cand am citit toate numerele din fisier
+        cout << contor_nr_pare; // se afiseaza cate numere pare am gasit
 
         f.close();
         return 0;
