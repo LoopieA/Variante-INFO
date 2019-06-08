8b0c51b1dde4ce34095b719d9fed53c2cd161641
Luchian67 <luchian.gh28@yahoo.com>
2019-06-08 11:32:48 +0300

    Add Vbac17 S2 E5

--- Vbac - 2017/Vbac17S3E3 - Luchian.cpp
+++ Vbac - 2017/Vbac17S3E3 - Luchian.cpp
@@ -3,51 +3,48 @@
 using namespace std;
 
 /*
-Subprogramul duplicare are un singur parametru, n, prin care primește un număr
-natural (nÎ[1,10^4)). Subprogramul furnizează, prin același parametru, numărul obţinut
-din n prin inserarea, după fiecare cifră pară din scrierea lui, a unei cifre egale cu jumătate
-din aceasta.
-Scrieți definiția completă a subprogramului.
+Scrieţi un program C/C++ care citeşte de la tastatură un număr natural n (n [2,50]) şi
+cele n elemente ale unui tablou unidimensional, numere naturale din intervalul [0,10^4].
+Cel puțin două elemente ale tabloului sunt nenule. Programul modifică în memorie tabloul
+prin ordonarea descrescătoare a elementelor nenule ale acestuia, celelalte elemente
+păstrându-şi poziția iniţială, apoi afișează pe ecran elementele tabloului obținut, separate
+prin câte un spațiu.
 */
-
-void duplicare(unsigned int &n)
-{
-    unsigned int v[11], lungime=0;
-    //mutam numarul n intr-un vector
-    while (n != 0)
-    {
-        v[lungime] = n%10;
-        n = n/10;
-        lungime++;
-    }
-    //parcurgem numarul
-    for (int i=0; i<lungime; i++)
-    {
-        //cand gasim o cifra para
-        //mutam toate elementele pana la cifra respectiva impreuna cu cifra o pozitie in dreapta
-        //iar pe pozitia ramasa libera se introduce valoarea ceruta (jumatate din valoarea cifrei pare)
-        if (v[i]%2 == 0)
-        {
-            for (int j=lungime-1; j>=i; j--)
-            {
-                v[j+1] = v[j];
-            }
-            v[i] = v[i+1]/2;
-            lungime++;
-            i++;
-        }
-    }
-    //reconstruim numarul
-    for (int i=lungime-1; i>=0; i--)
-    {
-        n = n*10 + v[i];
-    }
-
-}
 
 int main()
 {
-    unsigned int n; cin >> n;
-    duplicare(n);
-    cout << n;
+    unsigned int n, v[51], aux;
+    cin >> n;
+
+    //verificam daca n este intre 2 si 50
+    if (n < 2 || n > 50)
+        return 0;
+
+    //citim sirul si il introducem intr-un vector
+    for (int i=0; i<n; i++)
+        cin >> v[i];
+
+    //parcurgem sirul
+    for (int i=0; i<n-1; i++)
+    {
+        //daca numarul pe care am ajuns este 0, trecem la urmatorul numar
+        if (v[i] == 0)
+            continue;
+        //sortam numerele diferite de 0
+        for (int j=i; j<n; j++)
+        {
+            if (v[j] != 0 && v[i] < v[j])
+            {
+                aux = v[i];
+                v[i] = v[j];
+                v[j] = aux;
+            }
+        }
+    }
+
+    //afisam sirul
+    for (int i=0; i<n; i++)
+        cout << v[i] << " ";
+
+    return 0;
 }
