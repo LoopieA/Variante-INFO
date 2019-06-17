4a3bf89d6e1bb012a5538dc4c056c6e3bc522dd5
Luchian67 <luchian.gh28@yahoo.com>
2019-06-17 18:31:00 +0300

    Add VModel2017 S2 E5

---
+++ VModel - 2017/Vmodel2017S2E5 - Luchian.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+/*
+Un text are cel mult 100 de caractere, iar cuvintele sale sunt formate doar din litere mici ale
+alfabetului englez si sunt separate prin câte un spațiu.
+Scrieti un program C/C++ care citeste de la tastatură un text de tipul precizat mai sus si îl
+transformă în memorie prin înlocuirea fiecărui cuvânt format din număr par de litere cu
+simbolul #. Programul afisează pe ecran textul obtinut sau mesajul nu exista dacă textul
+citit nu conține astfel de cuvinte.
+*/
+int main()
+{
+    char s[100];
+    char *token;
+    cin.getline(s, 100);
+
+    token = strtok(s, " ");
+    while (token != NULL)
+    {
+        if (strlen(token)%2 == 0)
+            cout << "# ";
+        else
+            cout << token << " ";
+        token = strtok(NULL, " ");
+    }
+}
